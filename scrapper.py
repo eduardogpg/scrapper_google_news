1620175c3eda42c93a021ee8a6bace37d2f85918
Eduardo Ismael <eduardo78d@gmail.com>
2016-09-08 14:22:06 -0500

    Threads

--- scrapper.py
+++ scrapper.py
@@ -5,41 +5,34 @@ from datetime import datetime
 import threading
 
 GOOGLE_NEWS_URL = 'https://news.google.com.mx/'
-CUSTOME_TARGET = 'www.eluniversal.com/'
+CUSTOME_TARGET = 'www.eluniversal.com'
 
-
-class Article(object):
-	def __init__(self, title, url):
-		self.title = title
-		self.url = url
-		self.release_date = datetime.now()
-
-
-def get_container(url, tag, **kwgars):
-	request = request.get(url)
+def get_beautiful_soup(url):
+	request = requests.get(url)
 	if request.status_code == 200:
-		html = BeautifulSoup(request.text, "html.parser")
-		containers = html.find_all('h2', **kwgars)
-		return container
-	return None
+		return BeautifulSoup(request.text, "html.parser")
 
-def spider(pos, container):
-	title = container.find('span', {'class': 'titletext'}).getText()
-	url = container.find('a').get('href')
-	new_article = Article(title, url)
-	
+def set_spider(article):
+	title = article.find('span', {'class': 'titletext'}).getText()
+	url = article.find('a').get('href')
+	if CUSTOME_TARGET in url:
+		soup = get_beautiful_soup(url)
+		if soup is not None:
+			author = soup.find('div', {'class': 'field-item even'}).getText()
+			container = soup.find('div', {'class': 'pane-content'})
+			paragraphs = soup.container('p')
+			
+			for paragraph in paragraphs:
+				print paragraph
+			
+
 def scrap_site():
-	request = requests.get(GOOGLE_NEWS_URL)
-	status = request.status_code
-	
-	if status == 200:
-		html = BeautifulSoup(request.text, "html.parser")
-		containers_ = html.find_all('h2',{'class': 'esc-lead-article-title'})
-		
-		for pos, container in enumerate(containers_):
-			sender = threading.Thread(name='spider', target=spider, args=(pos,container,))
+	soup = get_beautiful_soup(GOOGLE_NEWS_URL)
+	if soup is not None:
+		articles = soup.find_all('h2', {'class': 'esc-lead-article-title'})
+		for article in articles:
+			sender = threading.Thread(name='set_spider', target=set_spider, args=(article,))
 			sender.start()
-
 
 if __name__ == '__main__':
 	scrap_site()
